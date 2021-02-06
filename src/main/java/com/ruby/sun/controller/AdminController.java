289666fe2d8c655c1b912809b7d5f6bbe5fa3cc1
ruby.sun <ruby.sun@cgi.com>
2021-02-05 20:21:50 -0500

    Spring Security

--- src/main/java/com/ruby/sun/controller/AdminController.java
+++ src/main/java/com/ruby/sun/controller/AdminController.java
@@ -2,11 +2,27 @@ package com.ruby.sun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
-    @GetMapping("/login")
+    @GetMapping("/home")
     public String getLoginPage(){
+        return "login";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "hello";
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
         return "login";
     }
 }
